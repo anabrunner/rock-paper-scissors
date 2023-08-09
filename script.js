10c1a413f31ee9f0362d75ad0bc4beb389e43228
anabrunner <126997850+anabrunner@users.noreply.github.com>
2023-08-08 21:12:24 -0700

    Add pseudocode for game code.

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//JavaScript for the Rock Paper Scissor game
+// JavaScript for the Rock Paper Scissor game
+
+// Computer will make a selection for its play (rock, paper, or scissors - random)
+// User will make a selection for their play
+// Determine who won the round
+// Tally the score
+// Report the winner and loser at the end of all rounds
