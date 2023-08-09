c9df39d615725103ce3a20e5cd00d17f25982710
anabrunner <126997850+anabrunner@users.noreply.github.com>
2023-08-08 22:56:05 -0700

    Correct win conditions and add validation check to player input.

--- script.js
+++ script.js
@@ -10,8 +10,7 @@ console.log(computerChoice);
 // User will make a selection for their play. Text will be input into a prompt and converted to an integer corresponding to the choice.
 // This is similar to the computer choice above.
 function getPlayerSelection() {
-    let playerSelection = prompt("Choose your weapon: rock, paper, or scissors?");
-    playerSelection.toLowerCase();
+    let playerSelection = prompt("Choose your weapon: rock, paper, or scissors?").toLowerCase();
     if (playerSelection === "rock") {
         return 1;
     } else if (playerSelection === "paper") {
@@ -20,18 +19,25 @@ function getPlayerSelection() {
         return 3;
     } else {
         alert("Please enter a valid weapon!");
+        return getPlayerSelection();
     }
 }
 
 // Determine who won the round
 function getWinner(computerChoice, playerChoice) {
-    let result;
+    let win = "You have won this round!";
+    let lose = "You have lost this round!";
+    let draw = "It's a draw!";
     if (computerChoice === playerChoice) {
-        return result = "It's a draw!";
-    } else if ((computerChoice === 1 && playerChoice === 3) || (computerChoice > playerChoice)) {
-        return result = "You have lost this round!";
+        return draw;
+    } else if (computerChoice === 1) {
+        return playerChoice === 2 ? win : lose;
+    } else if (computerChoice === 2) {
+        return playerChoice === 3 ? win : lose;
+    } else if (computerChoice === 3) {
+        return playerChoice === 1 ? win : lose;
     } else {
-        return result = "You have won this round!";
+        alert("Ops! Something has gone wrong!");
     }
 }
 
