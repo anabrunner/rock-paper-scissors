82aafe7221381e9e0afb0ca240d438e4e0ca8592
anabrunner <126997850+anabrunner@users.noreply.github.com>
2023-08-08 21:28:53 -0700

    Add function for computer to make selection.

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-// JavaScript for the Rock Paper Scissor game
+// JavaScript for the Rock Paper Scissors game
 
 // Computer will make a selection for its play (rock, paper, or scissors - random)
+function getComputerChoice() {
+    let randomChoice = Math.floor(Math.random() * 3) + 1;
+    let computerChoice = "";
+    if (randomChoice === 1) {
+        return computerChoice = "rock";
+    } else if (randomChoice === 2) {
+        return computerChoice = "paper";
+    } else {
+        return computerChoice = "scissors";
+    }
+}
+console.log(getComputerChoice());
 // User will make a selection for their play
 // Determine who won the round
 // Tally the score
